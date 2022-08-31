20072a8fd4e92e92aeff378892fc416e93324a1d
Erik Smith <github@codeblind.io>
2022-08-31 17:23:47 -0400

    List available questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "fontsource-roboto";
+import { AppBar, Toolbar, Container, Typography, Box } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+import questions from "./prop/questions.json";
+import "./App.css";
+
+type TQuestion = {
+  text: string;
+  flip?: boolean;
+};
+
+const theme = createTheme({
+  typography: {
+    h1: {
+      fontSize: "1.6rem",
+    },
+  },
+});
+
+const Question = ({
+  qid,
+  question,
+}: {
+  qid: string;
+  question: TQuestion;
+}): JSX.Element => {
+  return (
+    <p>
+      {qid}. {question.text} {question.flip === true ? "(X)" : ""}
+    </p>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Container maxWidth="md">
+          <AppBar position="sticky">
+            <Toolbar>
+              <Typography variant="h1">
+                Oxford Happiness Questionnaire
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Box component="main" sx={{ padding: 1.5 }}>
+            {Object.entries(questions.questions).map(
+              ([qid, value]: [string, TQuestion]) => (
+                <Question key={qid} qid={qid} question={value} />
+              )
+            )}
+          </Box>
+        </Container>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
